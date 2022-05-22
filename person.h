27932cc16c6f91789a3c8e7e39a26fbc732481de
LSY123LOVE <2213046975@qq.com>
2022-05-23 07:32:02 +0800

    初始化

---
+++ person.h
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: person.h
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2022年05月23日 星期一 07时19分57秒
+ ************************************************************************/
+#ifndef PERSON_H
+#define PERSON_H
+
+#include<iostream>
+using namespace std;
+
+#endif
