32297b89b35b2debb218cb685a205d18c68f4ec7
LSY123LOVE <2213046975@qq.com>
2022-05-22 17:14:54 +0800

    这是一个测试

---
+++ test.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: test.cpp
+	> Author: amoscykl
+	> Mail: amoscykl@163.com 
+	> Created Time: 2022年05月22日 星期日 17时13分51秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout << "hello world !" << endl;
+	return 0;
+}
